62099080f7752c4b1a2c71de75c2750b41c55cec
manticorevault <artur_seilah@hotmail.com>
2021-03-25 09:54:15 -0300

    Added some comments to the blockchain.js code

--- blockchain.js
+++ blockchain.js
@@ -1,10 +1,12 @@
 const singleBlock = require("./singleBlock");
 
+// Create the blockchain class to add the first block as genesis
 class Blockchain {
     constructor() {
         this.chain = [singleBlock.genesis()];
     }
 
+    // Create the addBlock function to get data as props and add to the blockchain array
     addBlock({ data }) {
         const newBlock = singleBlock.mineNewBlock({
             lastBlock: this.chain[this.chain.length - 1],
