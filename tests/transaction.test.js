f3dc74926e6259d28116f5cb690f48457ee0ee2e
manticorevault <artur_seilah@hotmail.com>
2021-05-28 11:38:11 -0300

    Added the test file for the transaction

---
+++ tests/transaction.test.js
@@ -0,0 +1,34 @@
+const Wallet = require("../wallet");
+const Transaction = require ("../wallet/transaction");
+
+describe("Transaction", () => {
+    let transaction, senderWallet, recipient, amount;
+
+    beforeEach(() => {
+        senderWallet = new Wallet();
+        recipient = "recipient-public-key";
+        amount = 100;
+
+        transaction = new Transaction({ senderWallet, recipient, amount });
+    });
+
+    
+    it("has an `id`", () => {
+       expect(transaction).toHaveProperty("id"); 
+    });
+
+    describe("outputMap", () => {
+        it("has an `outputMap`", () => {
+            expect(transaction).toHaveProperty("outputMap")
+        });
+
+        it("outputs the amount to the recipient", () => {
+            expect(transaction.outputMap[recipient]).toEqual(amount);
+        });
+
+        it("outputs the remaining balance for the `senderWallet`", () => {
+            expect(transaction.outputMap[senderWallet.publicKey])
+                .toEqual(senderWallet.balance - amount);
+        });
+    });
+});
