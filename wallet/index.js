0f482a01fe9c8699f364e6c9ec6fa24cd03f324e
manticorevault <artur_seilah@hotmail.com>
2021-05-28 11:28:00 -0300

    Added the code to verify valid signatures and disqualify invalid signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,14 +1,19 @@
 const { STARTING_BALANCE } = require("../blockchain/config");
 const { ec } = require("../utils");
+const hashing = require("../utils/hashing");
 
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
 
-        this.publicKey = keyPair.getPublic().encode("hex");
+        this.publicKey = this.keyPair.getPublic().encode("hex");
     }
-}
+
+    sign(data) {
+        return this.keyPair.sign(hashing(data))
+    }
+};
 
 module.exports = Wallet;
