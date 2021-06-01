cfcc8d65097070c77d3319743e66854709eb5682
manticorevault <artur_seilah@hotmail.com>
2021-06-01 13:03:23 -0300

    Added the tests to check if the amount in a transaction is valid

--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("../wallet/index");
+const Transaction = require("../wallet/transaction");
 const { verifySignature } = require("../utils")
 
 describe("Wallet", () => {
@@ -39,6 +40,38 @@ describe("Wallet", () => {
                     signature: new Wallet().sign(data)
                 })
             ).toBe(false);
+        });
+    });
+
+    describe("createTransaction", () => {
+        describe("and the amount exceeds the balance", () => {
+            it("throws an error", () => {
+                expect(() => wallet.createTransaction({ amount: 999999, recipient: "test-address" }))
+                    .toThrow("Amount exceeds balance!");
+            });
+        });
+
+        describe("and the amount is valid", () => {
+
+            let transaction, amount, recipient;
+
+            beforeEach(() => {
+                amount = 50,
+                recipient = "test-address",
+                transaction = wallet.createTransaction({ amount, recipient })
+            })
+
+            it("creates an instance of `Transaction`", () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            });
+
+            it("matches the transaction input with the wallet", () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey);
+            });
+
+            it("outputs the amount of the recipient", () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount);
+            });
         });
     });
 });
