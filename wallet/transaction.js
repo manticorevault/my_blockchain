498be6bf32443a4c0bb04bd0eda8f8b20d6af3c3
manticorevault <artur_seilah@hotmail.com>
2021-06-01 11:50:34 -0300

    Added the static function to transaction.js to pass all previous established tests

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid/v1");
+const { verifySignature } = require("../utils");
 
 class Transaction {
     constructor({ senderWallet, recipient, amount}) {
@@ -24,6 +25,25 @@ class Transaction {
             address: senderWallet.publicKey,
             signature: senderWallet.sign(outputMap)
         };
+    }
+
+    static validTransaction(transaction) {
+        const { input: { address, amount, signature }, outputMap } = transaction;
+
+        const outputTotal = Object.values(outputMap)
+            .reduce((total, outputMap) => total + outputMap);
+    
+        if (amount != outputTotal) {
+            console.error(`Invalid transaction from ${address}`);
+            return false;
+        }
+
+        if (!verifySignature({ publicKey: address, data: outputMap, signature })) {
+            console.error(`invalid signature from ${address}`);
+            return false;
+        }
+
+        return true
     }
 }
 
