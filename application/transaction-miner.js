262816f60889e8651249f26b4e24d2acd49c084e
manticorevault <artur_seilah@hotmail.com>
2021-06-09 11:29:40 -0300

    Added the constructor to the TransactionMiner class, as well as a mineTransaction function and its TODOs

--- application/transaction-miner.js
+++ application/transaction-miner.js
@@ -1,6 +1,22 @@
 class TransactionMiner {
-    mineTransaction() {
+    constructor(blockchain, transactionPool, wallet, pubsub) {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.pubsub = pubsub;
 
+    }
+
+    mineTransaction() {
+        //TODO: Get all the valid transactions from the transaction pool
+
+        //TODO: Reward the miners
+
+        //TODO: Attach a block to the blockchain, containing all the transactions
+
+        //TODO: Broadcast the information and update the blockchain
+
+        //TODO: Clear the pool
     }
 }
 
