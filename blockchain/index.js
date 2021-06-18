8e362b0539fc2fb9c90680301e85092a92361497
manticorevault <artur_seilah@hotmail.com>
2021-06-18 11:53:25 -0300

    Fixed a bug validation to handle some edge cases in the backend

--- blockchain/index.js
+++ blockchain/index.js
@@ -20,7 +20,7 @@ class Blockchain {
         this.chain.push(newBlock);
     }
 
-    replaceChain(chain, onSuccess) {
+    replaceChain(chain, validateTransactions, onSuccess) {
         if (chain.length <= this.chain.length) {
             console.error("The new chain must be longer")
             return;
@@ -28,6 +28,11 @@ class Blockchain {
 
         if (!Blockchain.isValidChain(chain)) {
             console.error("The new chain must be valid")
+            return;
+        }
+
+        if (validateTransactions && !this.validTransactionData({ data })) {
+            console.error("The chain must be valid");
             return;
         }
 
